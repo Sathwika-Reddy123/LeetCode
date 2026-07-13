7ef7d97e7da91aebd54135ce3f912c75f62dc616
Sathwika-Reddy123 <battusathwikareddy200@gmail.com>
2026-07-13 10:00:49 +0530

    Time: 15 ms (32.79%), Space: 19.7 MB (68.94%) - LeetHub

---
+++ 0051-n-queens/0051-n-queens.py
@@ -0,0 +1,27 @@
+class Solution:
+    def solveNQueens(self, n: int) -> List[List[str]]:
+        rows,cols,negD,posD=set(),set(),set(),set()
+        board=[['.']*n for i in range(n)]
+        res=[]
+        def bt(i):
+            if i==n:
+                res.append([''.join(p) for p in board])
+                return
+            for j in range(n):
+                if i in rows or j in cols or  i+j in posD or i-j in negD or board[i][j]=='Q':
+                    continue
+                board[i][j]="Q"
+                rows.add(i),
+                cols.add(j),
+                posD.add(i+j),
+                negD.add(i-j), 
+                      
+                bt(i+1)
+                board[i][j]='.'
+                rows.discard(i)
+                cols.discard(j)
+                posD.discard(i+j)
+                negD.discard(i-j)
+        bt(0)
+        return res
+               
