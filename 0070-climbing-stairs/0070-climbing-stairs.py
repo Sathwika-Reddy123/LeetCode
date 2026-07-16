2b5c1cb6c2ba91e64f821ec8d29e8e4c146637a4
Sathwika-Reddy123 <battusathwikareddy200@gmail.com>
2026-07-16 09:18:10 +0530

    Time: 0 ms (100%), Space: 19.3 MB (16.54%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,11 +1,26 @@
 class Solution:
+    @cache
     def climbStairs(self, n: int) -> int:
-        if n==1:
+        """if n==1:
             return 1
         if n==2:
             return 2
         dp=[1,2]
         for i in range(2,n):
             dp.append(dp[-1]+dp[-2])
-        return dp[-1]        
+        return dp[-1]  
+        if n<=1:
+            return 1
+        return self.climbStairs(n-1)+self.climbStairs(n-2)   """   
+        l=[-1]*(n+1)
+        l[0]=1
+        l[1]=1
+        def f(n):
+            if n<=1:
+                return 1
+            if l[n]!=-1:
+                return l[n]
+            l[n]=f(n-1)+f(n-2)
+            return l[n]
+        return f(n)
         
